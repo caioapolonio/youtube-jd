ceccb327941db3c80345a4f9fa4623816fbb2ebc
caioapolonio <cvmaaa1@gmail.com>
2024-06-27 11:19:31 -0300

    style: add App.js style

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  background-color: var(--background-color-grey);
+`;
