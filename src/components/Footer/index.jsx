f87140e12e60d787a5c5ad4826ce9c6bb6ed5a3c
Lucas Valdemar <lucas_valdemar@outlook.com>
2024-07-01 20:30:50 -0300

    footer imgs sizes fixed

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,32 +8,42 @@ import add from "../../assets/footer/Add.svg";
 const Footer = () => {
   return (
     <footer className={style.Footer}>
-      <div className={style.container_icone}>
-        <img className={style.footer_imgs} src={home} alt="Icone de Home" />
-        <h6>home</h6>
-      </div>
-      <div className={style.container_icone}>
-        <img className={style.footer_imgs} src={short} alt="Icone de Shorts" />
-        <h6>Shorts</h6>
-      </div>
-      <div>
-        <img className={style.footer_imgs} src={add} alt="Icone de Add" />
-      </div>
-      <div className={style.container_icone}>
-        <img
-          className={style.footer_imgs}
-          src={subscription}
-          alt="Icone de subscriptions"
-        />
-        <h6>Subscriptions</h6>
-      </div>
-      <div className={style.container_icone}>
-        <img
-          className={style.footer_imgs}
-          src={library}
-          alt="Icone de Library"
-        />
-        <h6>Library</h6>
+      <div className={style.footer_container}>
+        <div className={style.container_icone}>
+          <img className={style.footer_imgs} src={home} alt="Icone de Home" />
+          <h6>home</h6>
+        </div>
+        <div className={style.container_icone}>
+          <img
+            className={style.footer_imgs}
+            src={short}
+            alt="Icone de Shorts"
+          />
+          <h6>Shorts</h6>
+        </div>
+        <div>
+          <img
+            className={`${style.container_icone} ${style.more_container}`}
+            src={add}
+            alt="Icone de Add"
+          />
+        </div>
+        <div className={style.container_icone}>
+          <img
+            className={style.footer_imgs}
+            src={subscription}
+            alt="Icone de subscriptions"
+          />
+          <h6>Subscriptions</h6>
+        </div>
+        <div className={style.container_icone}>
+          <img
+            className={style.footer_imgs}
+            src={library}
+            alt="Icone de Library"
+          />
+          <h6>Library</h6>
+        </div>
       </div>
     </footer>
   );
