4be4a3d689eeca6fe511b4767a0fa6260822cae7
Lucas Valdemar <lucas_valdemar@outlook.com>
2024-07-01 16:54:05 -0300

    thumb sizes adjusted

--- src/components/YoutubeVideos/index.jsx
+++ src/components/YoutubeVideos/index.jsx
@@ -11,45 +11,44 @@ const YoutubeVideo = ({ videoID }) => {
     ? {
         objectPosition: data.thumbSettings.object_position,
         height: data.thumbSettings.height,
+        maxHeight: data.thumbSettings.maxHeight,
       }
     : {};
 
   if (loading) return <p>Carregando...</p>;
   if (error) return <p>Erro: {error.message}</p>;
   return (
-    <div>
-      <div className={style.video_container}>
+    <div className={style.video_container}>
+      <img
+        className={`${style.video_thumb} ${style.hover_itens}`}
+        src={data.videoThumb}
+        alt="videoThumb"
+        style={thumbSettings}
+      />
+      <div className={style.video_infos_container}>
         <img
-          className={`${style.video_thumb} ${style.hover_itens}`}
-          src={data.videoThumb}
-          alt="videoThumb"
-          style={thumbSettings}
+          className={`${style.user_avatar} ${style.hover_itens}`}
+          src={data.channelAvatar}
+          alt="channelAvatar"
         />
-        <div className={style.video_infos_container}>
-          <img
-            className={`${style.user_avatar} ${style.hover_itens}`}
-            src={data.channelAvatar}
-            alt="channelAvatar"
-          />
-          <div className={style.video_infos}>
-            <div className={`${style.Title} ${style.hover_itens}`}>
-              {data.videoTitle}
-            </div>
-            <div className={`${style.user_view_date} ${style.hover_itens}`}>
-              <p>{data.channelName}</p>
-              <img src={divider} alt="divider" />
-              <p>{data.videoViews}</p>
-              <img src={divider} alt="divider" />
-
-              <p>{data.videoDate}</p>
-            </div>
+        <div className={style.video_infos}>
+          <div className={`${style.Title} ${style.hover_itens}`}>
+            {data.videoTitle}
           </div>
-          <img
-            className={`${style.menu_icon} ${style.hover_itens}`}
-            src={menuIcon}
-            alt="menuIcon"
-          />
+          <div className={`${style.user_view_date} ${style.hover_itens}`}>
+            <p>{data.channelName}</p>
+            <img src={divider} alt="divider" />
+            <p>{data.videoViews}</p>
+            <img src={divider} alt="divider" />
+
+            <p>{data.videoDate}</p>
+          </div>
         </div>
+        <img
+          className={`${style.menu_icon} ${style.hover_itens}`}
+          src={menuIcon}
+          alt="menuIcon"
+        />
       </div>
     </div>
   );
